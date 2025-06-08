9c2c0368d4ab33137ad71c879614b8ddf9a0dd12
valiyyaddin <eliyevveliyeddin3@gmail.com>
2025-06-08 13:07:04 +0400

    Initial commit for english dictionary app

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+   return  render_template("home.html")
+
+@app.route('/api/v1//<word>')
+def about(word):
+    definition=word.capitalize()
+    return {
+        "definition"  : definition,
+        'word'  :  word
+     }
+
+
+if __name__  == "__main__":
+    app.run(debug=True)
