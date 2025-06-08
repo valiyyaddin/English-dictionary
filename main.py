46636743ea0d496b0163d2eafe344d28bdf07429
valiyyaddin <eliyevveliyeddin3@gmail.com>
2025-06-08 18:21:50 +0400

    Update code, display definition

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 from flask import Flask, render_template
+import pandas  as pd
+
+
+df = pd.read_csv("dictionary.csv")
 
 app=Flask(__name__)
+
+
 
 @app.route("/")
 def home():
    return  render_template("home.html")
 
-@app.route('/api/v1//<word>')
+@app.route('/api/v1/<word>/')
 def about(word):
-    definition=word.capitalize()
-    return {
-        "definition"  : definition,
-        'word'  :  word
-     }
+
+    definition=df.loc[df["word"] == word]["definition"].squeeze()
+    result_dictionary={
+        "definition": definition,
+        "word": word
+                       }
+
+
+    return  result_dictionary
 
 
 if __name__  == "__main__":
